881d7db08c27989c2daee09ec3741578b4e66297
hugur@hotmail.com <gantep27>
2017-11-28 01:08:46 +0000

    items filter is added

---
+++ src/components/ItemsFilter/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import './styles.css';
+
+export const ItemsFilter=({})=>{
+  return(
+    <div>
+    </div>
+  )
+}
